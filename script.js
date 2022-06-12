95a7fcf807aba592d76924a1382239477d702a6d
Kavya-25 <90210074+Kavya-25@users.noreply.github.com>
2022-06-12 14:36:25 +0530

    modes functionality added

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let addBtn=document.getElementById("addBtn");
 showNotes();
 addBtn.addEventListener("click",addNote)
+let cont=document.querySelector(".container")
+let nav=document.querySelector('nav')
+let mode=document.getElementById("mode")
 
 function addNote(){
     let notes=localStorage.getItem("New note");
@@ -75,4 +78,25 @@ search.addEventListener("input",()=>{
    })
 })
 
-  
+let modeBtn=document.getElementById('mode');
+modeBtn.addEventListener('click',()=>{
+    if(modeBtn.innerText=="Light Mode"){
+        modeBtn.innerText="Dark Mode";
+        document.body.style.background="linear-gradient(to right, #8e9eab, #eef2f3)"
+        document.body.style.color="black"
+        addTitle.style.color="black"
+        addTxt.style.color="black"
+        nav.style.backgroundColor="rgb(65, 161, 140)"
+        mode.style.borderColor="white"
+        
+    }
+    else if(modeBtn.innerText=="Dark Mode"){
+        modeBtn.innerText="Light Mode"
+        document.body.style.background="linear-gradient(to left , #000000, #434343)"
+        document.body.style.color="white" 
+         addTitle.style.color="white"
+        addTxt.style.color="white"
+        nav.style.backgroundColor="rgb(32, 31, 31)"
+        mode.style.borderColor="rgb(65, 161, 140)"
+    }
+})
